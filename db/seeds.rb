f2853ec54fd2eee751b01f057c62fb80bd9a53ff
agoldvarg <agoldvarg@gmail.com>
2015-02-27 14:02:17 -0500

    working without regex

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,10 @@ amazing_women.each do |woman|
   wiki = WikiScraper.new(w.name)
   w.has_wiki_page = wiki.has_page?
   if w.has_wiki_page == true
-
-    # w.bio = wiki.scrape_bio + "  (Source: Wikipedia)"
-    # w.wiki_page_link = "https://en.wikipedia.org/wiki/#{w.name.gsub(" ", "_")}"
-    w.bio = wiki.scrape_bio.delete('/(\[.+\])/')+"  (Source: Wikipedia)"
+    w.bio = wiki.scrape_bio + "  (Source: Wikipedia)"
     w.wiki_page_link = "https://en.wikipedia.org/wiki/#{w.name.gsub(" ", "_")}"
+    # w.bio = wiki.scrape_bio.delete('/(\[.+\])/')+"  (Source: Wikipedia)"
+    # w.wiki_page_link = "https://en.wikipedia.org/wiki/#{w.name.gsub(" ", "_")}"
   else
     w.wiki_create_link = "https://en.wikipedia.org/w/index.php?title=#{w.name.gsub(" ", "_")}&action=edit"
   end

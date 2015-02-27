3d5e9763211d672f11e9263a4e264e47a04c43f6
rnackman <rachel.nackman@gmail.com>
2015-02-27 10:21:09 -0500

    Connect all variables to ERB template.

--- runtime.rb
+++ runtime.rb
@@ -4,10 +4,16 @@ require 'pry'
 class RunTime < Sinatra::Base
   get '/' do
     @woman = Individual.all.sample
-    # get_tweets = TweetGrabber.new
-    # get_tweets.populate(@woman)
-    # @tweets = tweets.all
-    @stat = WikiStats.new(@woman.name).scrape_stats.strip.gsub("_", " ")
+    if @woman.has_wiki_page
+      @stat = WikiStats.new(@woman.name).scrape_stats.strip.gsub("_", " ")
+      @header = @woman.name.upcase+" HAS A WIKIPEDIA PAGE"
+      @directive = "<a href='#{@woman.wiki_page_link}'>WHY DON'T YOU MAKE IT BETTER?</a>"
+    else
+      @header = @woman.name.upcase+" DOES NOT HAVE A WIKIPEDIA PAGE"
+      @directive = "<a href='#{@woman.wiki_create_link}'>WHY DON'T YOU MAKE HER ONE?</a>"
+    end
+    @twitter_intro = "Here's what people have been saying about her on Twitter:"
+
     erb :main
   end
 end

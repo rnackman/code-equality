29a513b3c68e2076a9f23c58c6ffb576c495481c
rnackman <rachel.nackman@gmail.com>
2015-02-28 12:58:52 -0500

    Connect :index to controller & create @all variable.

--- app.rb
+++ app.rb
@@ -8,6 +8,11 @@ class GraceHopperTime < Sinatra::Base
     erb :main
   end
 
+  get '/index' do
+    @all = Individual.all
+    erb :index
+  end
+
   get '/:name' do
     name = params[:name].gsub(/_/, ' ').split.map(&:capitalize).join(' ')
     @woman = Individual.find_by(name: name)
@@ -15,18 +20,14 @@ class GraceHopperTime < Sinatra::Base
     erb :main
   end
 
-  get '/:index' do
-    erb :index
-  end
-
   def build_display_material(woman)
-    if @woman.has_wiki_page
-      @stat = WikiStats.new(@woman.name).scrape_stats.strip.gsub("_", " ")
-      @header = @woman.name.upcase+" HAS A WIKIPEDIA PAGE"
-      @directive = "<a href='#{@woman.wiki_page_link}' target='_blank'>WHY DON'T YOU MAKE IT BETTER?</a>"
+    if woman.has_wiki_page
+      @stat = WikiStats.new(woman.name).scrape_stats.strip.gsub("_", " ")
+      @header = woman.name.upcase+" HAS A WIKIPEDIA PAGE"
+      @directive = "<a href='#{woman.wiki_page_link}' target='_blank'>WHY DON'T YOU MAKE IT BETTER?</a>"
     else
-      @header = @woman.name.upcase+" DOES NOT HAVE A WIKIPEDIA PAGE"
-      @directive = "<a href='#{@woman.wiki_create_link}' target='_blank'>WHY DON'T YOU MAKE HER ONE?</a>"
+      @header = woman.name.upcase+" DOES NOT HAVE A WIKIPEDIA PAGE"
+      @directive = "<a href='#{woman.wiki_create_link}' target='_blank'>WHY DON'T YOU MAKE HER ONE?</a>"
     end
     @twitter_intro = "Here's what people have been saying about her on Twitter:"
   end

6f1c97df91ea7d1d3e0fe4b4d3192978a76ce375
rnackman <rachel.nackman@gmail.com>
2015-02-28 11:38:51 -0500

    Fix RegEx in bio scraper.

--- db/seeds_test.rb
+++ db/seeds_test.rb
@@ -13,7 +13,7 @@ amazing_women.each do |woman|
   wiki = WikiScraper.new(w.name)
   w.has_wiki_page = wiki.has_page?
   if w.has_wiki_page == true
-    w.bio = wiki.scrape_bio + "  (Source: Wikipedia)"
+    w.bio = wiki.scrape_bio.gsub!(/\[[0-9]+\]/, '') + "  (Source: Wikipedia)"
     w.wiki_page_link = "https://en.wikipedia.org/wiki/#{w.name.gsub(" ", "_")}"
   else
     w.wiki_create_link = "https://en.wikipedia.org/w/index.php?title=#{w.name.gsub(" ", "_")}&action=edit"

adece24e5a0b4edfce83f61bafbd7e5e681bfbe6
rnackman <rachel.nackman@gmail.com>
2015-02-28 12:38:24 -0500

    Refactor app.rb

--- app.rb
+++ app.rb
@@ -4,22 +4,22 @@ class GraceHopperTime < Sinatra::Base
 
   get '/' do
     @woman = Individual.all.sample
-    if @woman.has_wiki_page
-      @stat = WikiStats.new(@woman.name).scrape_stats.strip.gsub("_", " ")
-      @header = @woman.name.upcase+" HAS A WIKIPEDIA PAGE"
-      @directive = "<a href='#{@woman.wiki_page_link}' target='_blank'>WHY DON'T YOU MAKE IT BETTER?</a>"
-    else
-      @header = @woman.name.upcase+" DOES NOT HAVE A WIKIPEDIA PAGE"
-      @directive = "<a href='#{@woman.wiki_create_link}' target='_blank'>WHY DON'T YOU MAKE HER ONE?</a>"
-    end
-    @twitter_intro = "Here's what people have been saying about her on Twitter:"
-
+    build_display_material(@woman)
     erb :main
   end
 
   get '/:name' do
     name = params[:name].gsub(/_/, ' ').split.map(&:capitalize).join(' ')
     @woman = Individual.find_by(name: name)
+    build_display_material(@woman)
+    erb :main
+  end
+
+  get '/:index' do
+    erb :index
+  end
+
+  def build_display_material(woman)
     if @woman.has_wiki_page
       @stat = WikiStats.new(@woman.name).scrape_stats.strip.gsub("_", " ")
       @header = @woman.name.upcase+" HAS A WIKIPEDIA PAGE"
@@ -29,8 +29,5 @@ class GraceHopperTime < Sinatra::Base
       @directive = "<a href='#{@woman.wiki_create_link}' target='_blank'>WHY DON'T YOU MAKE HER ONE?</a>"
     end
     @twitter_intro = "Here's what people have been saying about her on Twitter:"
-
-    erb :main
   end
-
 end
